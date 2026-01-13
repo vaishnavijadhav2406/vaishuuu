c12de00f5cf4980a6cf03e38ba82f6b6c483ef69
vaishnavijadhav2406 <jadhavvaish234@gmail.com>
2026-01-13 10:19:22 +0000

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,10 +1,29 @@
-import streamlit as st 
+import streamlit as st
 
-st.title("some basic commands like slider button etc")
+st.title("Simple Calculator")
 
-age = st.slider("enter your age",1,100)
-city= st.selectbox("chosse your city" , ["delhi"],["pune"],["mumbai"],["chennai"])
 
- if st.button("show details"):
-    st.write ("age" , age)
-    st.write ("city" , city)
+num1 = st.number_input("Enter first number", value=0)
+num2 = st.number_input("Enter second number", value=0)
+
+
+operation = st.selectbox(
+    "Choose operation",
+    ("Add", "Subtract", "Multiply", "Divide")
+)
+
+
+if st.button("Calculate"):
+    if operation == "Add":
+        result = num1 + num2
+    elif operation == "Subtract":
+        result = num1 - num2
+    elif operation == "Multiply":
+        result = num1 * num2
+    elif operation == "Divide":
+        if num2 == 0:
+            st.error("Cannot divide by zero")
+        else:
+            result = num1 / num2
+
+    st.success(f"Result: {result}")
